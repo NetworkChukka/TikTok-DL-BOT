1d8dd97b3e5e9e2b0aa1c2238ba5e3be8e4f3a85
NetworkChukka <89191830+NetworkChukka@users.noreply.github.com>
2022-08-01 21:15:20 +0530

    Tiktok

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import logging
 
 
 # TikTok Downloader API
-API = 'https://host.single-developers.software/tiktok?url='
+API = 'https://api.single-developers.software/tiktok?url='
 
 # Your BOT Token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
